8024d30ca90d3241f138ec944c06c411b9cb75dd
Sneha Rawat <sneharawat0901@gmail.com>
2025-12-01 22:00:08 +0530

    Initial commit

---
+++ scripts/events.js
@@ -0,0 +1,143 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const eventJson = localStorage.getItem("events");
+  let events = JSON.parse(eventJson);
+  let eventsTable = document.getElementById("events-table");
+  failed = [];
+
+  events.forEach((element) => {
+    let eventName = element["eventName"];
+    let eventId = element["eventId"];
+    let startDate = element["startDate"];
+    let endDate = element["endDate"];
+
+    let eventColumn = createRow(eventId, eventName, startDate, endDate);
+    eventsTable.appendChild(eventColumn);
+
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    let taskCount = tasks.filter((task) => task["eventId"] === eventId).length;
+
+    if (!localStorage.getItem("task-status-" + eventId)) {
+      let initialStatusArray = Array(taskCount).fill("Not Started");
+
+      localStorage.setItem(
+        "task-status-" + eventId,
+        JSON.stringify(initialStatusArray)
+      );
+    }
+
+    updateProgress(eventId);
+    checkFailure(startDate, eventId);
+  });
+
+  localStorage.setItem("failedTasks", JSON.stringify(failed));
+});
+
+function createRow(eventId, eventName, startDate, endDate) {
+  let idRow = document.createElement("td");
+  let nameRow = document.createElement("td");
+  let startDateRow = document.createElement("td");
+  let endDateRow = document.createElement("td");
+
+  let progressRow = document.createElement("td");
+  progressRow.id = "progress-" + eventId;
+  let actionRow = createAction(eventId, eventName);
+
+  idRow.textContent = eventId;
+  nameRow.textContent = eventName;
+  startDateRow.textContent = startDate;
+  endDateRow.textContent = endDate;
+  progressRow.textContent = "Not Started";
+
+  let eventColumn = document.createElement("tr");
+  eventColumn.id = eventId;
+
+  eventColumn.appendChild(idRow);
+  eventColumn.appendChild(nameRow);
+  eventColumn.appendChild(startDateRow);
+  eventColumn.appendChild(endDateRow);
+  eventColumn.appendChild(progressRow);
+  eventColumn.appendChild(actionRow);
+
+  return eventColumn;
+}
+
+function createAction(eventId, eventName) {
+  let actionRow = document.createElement("td");
+  let actionAnchor = document.createElement("a");
+  actionAnchor.onclick = function () {
+    goToTasks(eventId, eventName);
+  };
+  let actionButton = document.createElement("button");
+  actionButton.className = "action-button";
+
+  actionButton.textContent = "TASK";
+  actionAnchor.appendChild(actionButton);
+  actionRow.appendChild(actionAnchor);
+
+  return actionRow;
+}
+
+function goToTasks(eventId, eventName) {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks == null || tasks.length == 0) {
+    setWarningPopup("No tasks added");
+    return;
+  }
+  localStorage.setItem("eventId", eventId);
+  localStorage.setItem("eventName", eventName);
+  window.location.href = "./tasks.html";
+}
+
+function updateProgress(eventId) {
+  let progressRow = document.getElementById("progress-" + eventId);
+
+  let taskStatusesJson = localStorage.getItem("task-status-" + eventId);
+  if (taskStatusesJson) {
+    let taskStatuses = JSON.parse(taskStatusesJson);
+    if (taskStatuses.includes("In Progress")) {
+      progressRow.textContent = "In Progress";
+    } else if (taskStatuses.every((status) => status === "Complete")) {
+      progressRow.textContent = "Complete";
+    }
+  }
+}
+
+function checkFailure(startDateStr, eventId) {
+  let progressRow = document.getElementById("progress-" + eventId);
+  let startDate = new Date(startDateStr);
+  let taskStatusJson = localStorage.getItem("task-status-" + eventId);
+  let taskStatus = JSON.parse(taskStatusJson);
+  const todayDate = new Date();
+  if (startDate < todayDate) {
+    if (!taskStatus.every((status) => status === "Complete")) {
+      progressRow.textContent = "Failed";
+      progressRow.id = "";
+    }
+  }
+}
+
+function setWarningPopup(msg) {
+  document.getElementById("warning-msg").innerText = "MESSAGE\n" + msg;
+  document.getElementById("warning-popup").style.padding = "20px";
+  document.getElementById("warning-popup").style.border = "2px solid black";
+  document.getElementById("contents-div").style.opacity = "10%";
+
+  let closeButton = document.getElementById("close");
+  if (!closeButton) {
+    closeButton = document.createElement("input");
+    closeButton.className = "close-btn";
+    closeButton.id = "close";
+    closeButton.value = "CLOSE";
+    closeButton.type = "button";
+    closeButton.addEventListener("click", reverseInvoice);
+    document.getElementById("warning-popup").appendChild(closeButton);
+  }
+}
+
+function reverseInvoice() {
+  document.getElementById("warning-msg").innerText = "";
+  document.getElementById("close").remove();
+  document.getElementById("warning-popup").style.border = "none";
+  document.getElementById("contents-div").style.opacity = "100%";
+  document.getElementById("warning-popup").style.padding = "0px";
+}
---
+++ scripts/tasks.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const taskJson = localStorage.getItem("tasks");
+  tasks = JSON.parse(taskJson);
+  tasksTable = document.getElementById("tasks-table");
+  requiredEvent = localStorage.getItem("eventId");
+  requiredEventName = localStorage.getItem("eventName");
+  taskCount = 0;
+  statusArray = [];
+  setEventName();
+  tasks.forEach((element) => {
+    let taskId = element["eventId"];
+    if (taskId == requiredEvent) {
+      let taskName = element["taskName"];
+      let taskColumn = createRow(taskId, taskName);
+      tasksTable.appendChild(taskColumn);
+    }
+  });
+  if (localStorage.getItem("task-status-" + requiredEvent) == null) {
+    localStorage.setItem("task-status-" + requiredEvent, statusArray);
+  }
+  updateStatus();
+});
+
+function setEventName() {
+  document.getElementById("event-name").textContent = requiredEventName;
+}
+
+function createRow(taskId, taskName) {
+  let idRow = document.createElement("td");
+  let nameRow = document.createElement("td");
+  let statusRow = createStatusSelect();
+
+  idRow.textContent = taskId;
+  nameRow.textContent = taskName;
+
+  let taskColumn = document.createElement("tr");
+  taskColumn.id = taskId;
+
+  taskColumn.appendChild(idRow);
+  taskColumn.appendChild(nameRow);
+  taskColumn.appendChild(statusRow);
+
+  return taskColumn;
+}
+
+function createStatusSelect() {
+  taskCount += 1;
+  let statusRow = document.createElement("td");
+  let statusSelect = document.createElement("select");
+  statusSelect.id = "task" + taskCount;
+
+  let notStartedOption = document.createElement("option");
+  notStartedOption.textContent = "Not Started";
+  notStartedOption.value = "Not Started";
+
+  let progressOption = document.createElement("option");
+  progressOption.textContent = "In Progress";
+  progressOption.value = "In Progress";
+
+  let startedOption = document.createElement("option");
+  startedOption.textContent = "Complete";
+  startedOption.value = "Complete";
+
+  statusSelect.appendChild(notStartedOption);
+  statusSelect.appendChild(progressOption);
+  statusSelect.appendChild(startedOption);
+
+  statusRow.appendChild(statusSelect);
+  statusArray.push("Not Started");
+  return statusRow;
+}
+
+function confirmStatus() {
+  statusList = [];
+  let statusSelects = tasksTable.querySelectorAll("select");
+  statusSelects.forEach((select) => {
+    let status = select.value;
+    statusList.push(status);
+  });
+  let statusListJson = JSON.stringify(statusList);
+  console.log(statusListJson);
+  localStorage.setItem("task-status-" + requiredEvent, statusListJson);
+  window.location.href = "./events.html";
+}
+function updateStatus() {
+  let statusListJson = localStorage.getItem("task-status-" + requiredEvent);
+  let statusList = JSON.parse(statusListJson);
+
+  for (let i = 0; i < statusList.length; i++) {
+    let taskId = "task" + (i + 1);
+    let statusSelect = document.getElementById(taskId);
+    statusSelect.value = statusList[i];
+  }
+}
